b255617c850b212338269229aae6d027f631bab7
MrDoniz <alu0101217277@ull.edu.es>
2023-03-02 18:22:57 +0000

    Terminado

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -4,13 +4,33 @@ import * as estraverse from "estraverse";
 import * as fs from "fs/promises";
 
 export async function transpile(inputFile, outputFile) {
-  // Fill in the code here
+  let input = await fs.readFile(inputFile, 'utf-8')
+  const output = addLogging(input);
+  if (outputFile === undefined) {
+    return;
+  }
+  await fs.writeFile(outputFile, output)
 }
 
 export function addLogging(code) {
-  // Fill in the code here
+  const ast = espree.parse(code, {ecmaVersion:6, loc:true});
+  estraverse.traverse(ast, {
+    enter: function (node, parent) {
+      if (node.type === 'FunctionDeclaration' || 
+          node.type === 'FunctionExpression' || 
+          node.type === 'ArrowFunctionExpression') {
+          let location = node.loc.start;
+          addBeforeCode(node, location.line);
+      }
+    }
+  });
+  return escodegen.generate(ast);
 }
 
-function addBeforeCode(node) {
- // Fill in the code here
+function addBeforeCode(node, lines) {
+  const name = node.id ? node.id.name : '<anonymous function>';
+  const parameters = node.params.map(param => `\$\{${param.name}\}`);
+  const beforeCode = "console.log(`Entering " + name + "(" + parameters + ") at line " + lines + "`);";
+  const beforeNodes = espree.parse(beforeCode, {ecmaVersion:6}).body;
+  node.body.body = beforeNodes.concat(node.body.body);
 }
