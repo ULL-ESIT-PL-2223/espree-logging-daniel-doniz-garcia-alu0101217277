1eb895c0da863928069efebea4ef86d46e4e4c14
MrDoniz <alu0101217277@ull.edu.es>
2023-03-03 12:13:53 +0000

    Terminado

---
+++ test/data/logged6.js
@@ -0,0 +1,5 @@
+const getRectArea = function (width, height) {
+    console.log(`Entering <anonymous function>(${ width },${ height }) at line 1`);
+    return width * height;
+};
+getRectArea(2, 4);
